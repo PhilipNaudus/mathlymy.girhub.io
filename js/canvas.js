8360f70a2a8da0d47adb8ae64ae51ae8343fc3d3
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-06 12:04:51 +0800

    bug

--- js/canvas.js
+++ js/canvas.js
@@ -81,8 +81,8 @@ function addText(p, text) {
 	yScale = (canvas.height - 50 - 10) / (y_max - y_min);
 	xScale = (canvas.width - 50) / (x_max - x_min);
 
-	var x = 50 + (xScale * p[0] * gx_step); 
-	var y = 50 + (yScale * p[1] * gy_step) + 5; 
+	var x = 50 + (xScale * (p[0]+5) * gx_step); 
+	var y = 50 + (yScale * (p[1]+5) * gy_step) + 5; 
 	context.fillText(text, x-5, y+10);
 }
 
