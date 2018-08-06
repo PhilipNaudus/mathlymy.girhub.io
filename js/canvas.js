e5195a8ec58111daebeb03111d180a2c695809a1
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-06 11:51:52 +0800

    bug

--- js/canvas.js
+++ js/canvas.js
@@ -50,7 +50,6 @@ function initCanvas(xArr, x_step, yArr, y_step, append) {
 		var y = columnSize + (yScale * count * gy_step) + 5; 
 		//var y = columnSize + (yScale * 0 * y_step) + 5; 
 		context.fillText(setAppend(scale,append[1]), margin,y + margin);
-		context.fillText("X", 10,y + 10);
 
 		context.moveTo(rowSize,y)
 		context.lineTo(canvas.width,y)
