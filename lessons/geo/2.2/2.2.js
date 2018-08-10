3a6aabbd5339a021db9723526c27199548b850d2
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-10 11:58:06 +0800

    c

--- lessons/geo/2.2/2.2.js
+++ lessons/geo/2.2/2.2.js
@@ -4,20 +4,34 @@ function setQuestion()
 {
     var hypo = ["it rains", "I eat an apple", "my friend gives me a call", "I read a book", "my computer breaks", "I lose my keys", "I have money"];
     var concl = ["I will go to the store", "I will watch a movie", "I will go to my friend's house", "I will clean my room", "I will eat a banana", "I will do my homework", "I will close my book"];
+
+    var nhypo = ["it does not rains", "I will not eat an apple", "my friend does not give me a call", "I do not read a book", "my computer does not break", "I do not lose my keys", "I do not have money"];
+    var nconcl = ["I will not go to the store", "I will not watch a movie", "I will not go to my friend's house", "I will not clean my room", "I will not eat a banana", "I will not do my homework", "I will not close my book"];
     var ans;
     var vars = getRandom(0, 6, 2);
+    var fourFive = getRandom(4, 5, 1);
     switch(questionNum)
     {
         case 0:
         case 1:
-            quest = "Please enter the <b>hypothesis</b> and <b>conclusion</b> of the below conditional statement:<br />If "+hypo[vars[0]]+", then "+concl[vars[1]]+".";
+            quest = "Please enter the <b>hypothesis</b> and <b>conclusion</b> of the below conditional statement:<br /><b>If "+hypo[vars[0]]+", then "+concl[vars[1]]+".</b>";
 	    ans = [hypo[vars[0]], concl[vars[1]]];
 	    equationNum = 0;
 	    break;
         case 2:
         case 3:
-            quest = "Please enter the <b>converse</b> of the below conditional statement:<br />If "+hypo[vars[0]]+", then "+concl[vars[1]]+".";
+            quest = "Please enter the <b>converse</b> of the below conditional statement:<br /><b>If "+hypo[vars[0]]+", then "+concl[vars[1]]+".</b>";
 	    ans = ["If "+concl[vars[1]]+", then "+hypo[vars[0]]+"."];
+	    equationNum = 1;
+	    break;
+        case fourFive[0]:
+            quest = "Please examine the two conditional sentences below and determine whether the second one is a contrapositive or inverse of the first. Please enter <b>contrapositive</b> or <b>inverse</b>.<br /><b>If "+hypo[vars[0]]+", then "+concl[vars[1]]+".<br />If "+nhypo[vars[0]]+", then "+nconcl[vars[1]]+".</b>";
+	    ans = ["inverse"];
+	    equationNum = 1;
+	    break;
+	default:
+            quest = "Please examine the two conditional sentences below and determine whether the second one is a contrapositive or inverse of the first. Please enter <b>contrapositive</b> or <b>inverse</b>.<br /><b>If "+hypo[vars[0]]+", then "+concl[vars[1]]+".<br />If "+nconcl[vars[1]]+", then "+nhypo[vars[0]]+".</b>";
+	    ans = ["contrapositive"];
 	    equationNum = 1;
 	    break;
     }
