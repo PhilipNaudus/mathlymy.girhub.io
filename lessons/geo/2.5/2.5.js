f37caf2be7622391aefe6b942c7eed5924c45e21
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-14 09:07:13 +0800

    c

--- lessons/geo/2.5/2.5.js
+++ lessons/geo/2.5/2.5.js
@@ -44,10 +44,10 @@ function setQuestion()
 			+"<tr><td>"+((vars[0]*vars[3])+(vars[1]*vars[3]))+"x</td><td>"+signNumber(vars[2]*vars[3])+"</td><td>=</td><td>"+(vars[1]*vars[3])+"x</td><td>"+signNumber((vars[0]*vars[3]*vars[4])+(vars[2]*vars[3]))+"</td><td></td><td><select class='mathinput' id='i2'>"+options+"</select></td></tr>"
 
 			+"<tr><td>"+(vars[0]*vars[3])+"x</td><td>"+signNumber(vars[2]*vars[3])+"</td><td>=</td><td></td><td>"+((vars[0]*vars[3]*vars[4])+(vars[2]*vars[3]))+"</td></tr>"
-			+"<tr><td><input type='text' class='mathinput' id='i9' /></td><td></td><td>=</td><td><input type='text' class='mathinput' id='i9' /></td><td></td></tr>"
+			+"<tr><td><input type='text' class='mathinput' id='i9' /></td><td></td><td>=</td><td><input type='text' class='mathinput' id='i9' /></td><td></td><td></td><td><select class='mathinput' id='i2'>"+options+"</select></td></tr>"
 
 			+"<tr><td>"+(vars[0]*vars[3])+"x</td><td></td><td>=</td><td></td><td>"+(vars[0]*vars[3]*vars[4])+"</td></tr>"
-			+"<tr><td></td><td><input type='text' class='mathinput' id='i9' /></td><td>=</td><td></td><td><input type='text' class='mathinput' id='i9' /></td></tr>"
+			+"<tr><td></td><td><input type='text' class='mathinput' id='i9' /></td><td>=</td><td></td><td><input type='text' class='mathinput' id='i9' /></td><td></td><td><select class='mathinput' id='i2'>"+options+"</select></td></tr>"
 
 			+"<tr><td>x</td><td></td><td>=</td><td></td><td><input type='text' class='mathinput' id='i9' /></td></tr></table>", [[vars[4]]]]];
 }
