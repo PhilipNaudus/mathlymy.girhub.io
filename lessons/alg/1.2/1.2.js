74f48622ab0759f45b119cd9e65ce3bdfea7c41f
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-16 13:52:35 +0800

    b

--- lessons/alg/1.2/1.2.js
+++ lessons/alg/1.2/1.2.js
@@ -6,7 +6,7 @@ function setQuestion()
     var quest;
     var ans;
     var eq;
-    var vars = getRandom(2, 10, 4)
+    var vars = getRandom(2, 7, 4, [4, 6])
     switch(questionNum)
     {
         case 0:
