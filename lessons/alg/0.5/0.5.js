0a917505ce82c54c20c7c03b8b11a4daab26c3aa
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-16 08:35:51 +0800

    b

--- lessons/alg/0.5/0.5.js
+++ lessons/alg/0.5/0.5.js
@@ -27,23 +27,23 @@ function setQuestion()
 	    eq = "(<input type='number' class='mathinput' id='i0' />x + <input type='number' class='mathinput' id='i1' />) (<input type='number' class='mathinput' id='i2' />x + <input type='number' class='mathinput' id='i3' />)";
 	    ans = [vars[0], vars[1], vars[0], vars[1]];
             break;
-        case 5:
         case 6:
+        case 7:
             var posNeg = (Math.round(Math.random())==0)?"-":"+";
             var negPos = (posNeg=="-")?"+":"-";
 
-	    quest = "Please factor:<br />"+Math.pow(vars[0],3)+"x<sup>3</sup> "+posNeg+" "+Math.pow(vars[1],3)+"<br /><em>Hint:</em><sup>3</sup><div class='sqrt'><span class='overline'>"+Math.pow(vars[0],3)+"</span></div> = "+vars[0];
+	    quest = "Please factor:<br />"+Math.pow(vars[0],3)+"x<sup>3</sup> "+posNeg+" "+Math.pow(vars[1],3)+"<br /><em>Hint:</em> <sup>3</sup><div class='sqrt'><span class='overline'>"+Math.pow(vars[0],3)+"</span></div> = "+vars[0] + "and <sup>3</sup><div class='sqrt'><span class='overline'>"+Math.pow(vars[1],3)+"</span></div> = "+vars[1];
 	    eq = "(<input type='number' class='mathinput' id='i0' />x "+posNeg+" <input type='number' class='mathinput' id='i1' />) (<input type='number' class='mathinput' id='i2' />x<sup>2</sup> "+negPos+" <input type='number' class='mathinput' id='i3' />x + <input type='number' class='mathinput' id='i4' />)";
 	    ans = [vars[0], vars[1], (vars[0]*vars[0]), (vars[0]*vars[1]), (vars[1]*vars[1])];
             break;
-        case 7:
         case 8:
+        case 9:
             quest = "Please factor by grouping:<br />("+(vars[0]*vars[2])+"x<sup>2</sup> + "+(vars[1]*vars[2])+"x + "+(vars[0]*vars[3])+"x + "+(vars[1]*vars[3]);
 	    eq = "(<input type='number' class='mathinput' id='i0' />x + <input type='number' class='mathinput' id='i1' />) (<input type='number' class='mathinput' id='i2' />x + <input type='number' class='mathinput' id='i3' />)";
 	    ans = [vars[0], vars[1], vars[2], vars[3]];
             break;
-        case 9:
         case 10:
+        case 11:
             quest = "Please factor:<br />("+(vars[0]*vars[2])+"x<sup>2</sup> + "+((vars[1]*vars[2])+(vars[0]*vars[3]))+"x + "+(vars[1]*vars[3]);
 	    eq = "(<input type='number' class='mathinput' id='i0' />x + <input type='number' class='mathinput' id='i1' />) (<input type='number' class='mathinput' id='i2' />x + <input type='number' class='mathinput' id='i3' />)";
 	    ans = [vars[0], vars[1], vars[2], vars[3]];
@@ -108,4 +108,4 @@ function signNumber(theNumber)
     }
 }
 
-var numQuestions = 9;
+var numQuestions = 12;
