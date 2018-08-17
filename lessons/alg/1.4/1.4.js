82e80f840842b8a13dbd000c5f1a17369694ba92
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-17 09:19:39 +0800

    1.4

---
+++ lessons/alg/1.4/1.4.js
@@ -0,0 +1,78 @@
+//var steps = ["Step 1: Select the best equation", "Step 2: Enter numbers into the equation"];
+var videos = ["tyBiHyazMOc", "ST4Jq80MWFw", "asobS45OFJY", "ewukx8sw2D8"];
+
+function setQuestion()
+{
+    var quest;
+    var ans;
+    var eq;
+    switch(questionNum)
+    {
+        case 0:
+        case 1:
+            var vars = getRandom(1, 9, 3);
+            quest = "Please solve for x:<br /><div class='sqrt'><span class='overline'>"+(vars[0]*vars[0])+"x"+(vars[0]*vars[0]*vars[1])+" = "+(vars[0]*vars[2]);
+	    eq = "x = <input type='number' class='mathinput' id='i0' />";
+	    ans = [((vars[2]*vars[2]) - vars[1])];
+            break;
+    }
+
+    equationNum = 0;
+    document.getElementById("question").innerHTML = "Question "+(questionNum+1)+": "+quest;
+    equations = [["", eq, [ans]]];
+}
+
+function step1(ifResetScrolling)
+{
+    endLoading();
+    currentStep = 2;
+
+    document.getElementById("step").innerHTML = "";
+    document.getElementById("math").innerHTML = equations[equationNum][1];
+
+    addInputEventListeners();
+
+    if(ifResetScrolling) window.scrollTo(0, 0);
+    enableDisableButtons();
+}
+
+function giveHint()
+{
+    if(currentStep==1)
+    {
+        swal("Use the first equation if you are given two points and cannot see the graph of the line. Use the second equation if you are given the graph of the line. Use the third equation if you are confident in your ability to apply the Pythagorean Theorem in your head.");
+    } else
+    {
+        var numCorrect = new Array();
+        var studentSolution, input, sol;
+
+        for(var i=0; i<equations[equationNum][2].length; i++)
+        {
+            numCorrect[i] = testSolution(i);
+        }
+        studentSolution = indexOfMax(numCorrect);
+
+        sol = equations[equationNum][2][studentSolution];
+        for(var i=0; i<sol.length; i++)
+        {
+            input = document.getElementById("i"+i);
+            if(input.value != sol[i])
+            {
+                input.value = sol[i];
+                break;
+            }
+        }
+        checkChanged(input);
+    }
+}
+
+function signNumber(theNumber)
+{
+    if(theNumber >= 0){
+        return " + " + theNumber;
+    }else{
+        return " - " + Math.abs(theNumber);
+    }
+}
+
+var numQuestions = 6;
