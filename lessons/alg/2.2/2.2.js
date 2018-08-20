4ac88ed05db7a5d2a6ecfad5ddef4958cd006588
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-08-20 09:20:44 +0800

    b

--- lessons/alg/2.2/2.2.js
+++ lessons/alg/2.2/2.2.js
@@ -76,19 +76,19 @@ function setQuestion()
 			var y = new Array();
 			var vars = getRandom(-3,3,2,[0]);
 			var n = getRandom(0,3,1);
+			var even = getRandom(1,9,1,[2,4,6,8]);
+			var odd = getRandom(2,8,1,[3,5,7]);
 
 			for(var i=-5; i<5; i+=0.01)
 			{
 				switch(n[0])
 				{
 					case 0:
-						var even = getRandom(1,9,1,[2,4,6,8]);
 						x.push(i);
 						y.push(vars[0]*Math.pow(i,even[0])+vars[1]);
 						ans = ["x-axis"];
 						break;
 					case 1:
-						var even = getRandom(1,9,1,[2,4,6,8]);
 						y.push(i);
 						x.push(vars[0]*Math.pow(i,even[0])+vars[1]);
 						ans = ["y-axis"];
@@ -100,9 +100,8 @@ function setQuestion()
 						ans = ["origin"];
 						break;
 					case 3:
-						var odd = getRandom(2,8,1,[3,5,7]);
 						x.push(i);
-						y.push(vars[0]*Math.pow(i),odd[0]);
+						y.push(vars[0]*Math.pow(i,odd[0]));
 						ans = ["origin"];
 						break;
 				}
