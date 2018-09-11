c063195c63a8076b78adbc3fc3d709aadda72b5c
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-09-11 15:10:45 +0800

    b

--- lessons/geo/3.4/3.4.js
+++ lessons/geo/3.4/3.4.js
@@ -19,8 +19,8 @@ function setQuestion()
 			document.getElementById("question").innerHTML = "Question "+(questionNum+1)+": What is the slope of the line which passes through the points ("+vars[0]+","+vars[1]+") and ("+vars[2]+","+vars[3]+")?";
 			break;
 	}
-	var slopeSign = ((vars[0]-vars[2])/(vars[1]-vars[3]))>0?1:-1;
-	var ans = reduce(Math.abs(vars[0]-vars[2]), Math.abs(vars[1]-vars[3]));
+	var slopeSign = ((vars[1]-vars[3])/(vars[0]-vars[2]))>0?1:-1;
+	var ans = reduce(Math.abs(vars[1]-vars[3]), Math.abs(vars[0]-vars[2]));
 
 	if(ans[1]==1) equationNum = (practice)?1:3;
 	else equationNum = (practice)?0:2;
@@ -30,16 +30,16 @@ function setQuestion()
 			+"</span> = <span class='fraction'>"
                           +"<span class='fractop'><input type='number' class='mathinput' id='i4' /></span>"
 			  +"<span class='fracbot'><input type='number' class='mathinput' id='i5' /></span>"
-			+"</span>", [[vars[0], vars[2], vars[1], vars[3], ans[0]*slopeSign, ans[1]],
-		                     [vars[0], vars[2], vars[1], vars[3], ans[0], ans[1]*slopeSign],
-		                     [vars[2], vars[0], vars[3], vars[1], ans[0]*slopeSign, ans[1]],
-				     [vars[2], vars[0], vars[3], vars[1], ans[0], ans[1]*slopeSign]]],
+			+"</span>", [[vars[1], vars[3], vars[0], vars[2], ans[0]*slopeSign, ans[1]],
+		                     [vars[1], vars[3], vars[0], vars[2], ans[0], ans[1]*slopeSign],
+		                     [vars[3], vars[1], vars[2], vars[0], ans[0]*slopeSign, ans[1]],
+				     [vars[3], vars[1], vars[2], vars[0], ans[0], ans[1]*slopeSign]]],
                     ["", "<span class='fraction'>"
                           +"<span class='fractop'><input type='number' class='mathinput' id='i0' />-<input type='number' class='mathinput' id='i1' /></span>"
 			  +"<span class='fracbot'><input type='number' class='mathinput' id='i2' />-<input type='number' class='mathinput' id='i3' /></span>"
 			+"</span> = <input type='number' class='mathinput' id='i4' />",
-			            [[vars[0], vars[2], vars[1], vars[3], ans[0]*slopeSign],
-		                     [vars[2], vars[0], vars[3], vars[1], ans[0]*slopeSign]]],
+			            [[vars[1], vars[3], vars[0], vars[2], ans[0]*slopeSign],
+		                     [vars[3], vars[1], vars[2], vars[0], ans[0]*slopeSign]]],
 	            ["", "<span class='fraction'>"
                           +"<span class='fractop'><input type='number' class='mathinput' id='i0' /></span>"
 			  +"<span class='fracbot'><input type='number' class='mathinput' id='i1' /></span>"
