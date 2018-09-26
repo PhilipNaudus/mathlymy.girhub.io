be8a69d94b1a81a9ff08d1bf5747157fc6cbd76b
mathlyml <38436197+mathlyml@users.noreply.github.com>
2018-09-26 11:57:49 +0800

    b

--- lessons/geo/4.1/4.1.js
+++ lessons/geo/4.1/4.1.js
@@ -58,6 +58,7 @@ function setQuestion()
     }
 
     document.getElementById("question").innerHTML = "Question "+(questionNum+1)+": "+quest+"<br /><img style='width: 100px; height: auto;' src='/lessons/geo/4.1/"+img+"' />";
+    equationNum = 0;
 
     equations = [["", "<select class='mathinput' id='i0'>"+options+"</select>", [[ans]]]];
 }
@@ -70,51 +71,10 @@ function step1(ifResetScrolling)
     document.getElementById("step").innerHTML = "";
     document.getElementById("math").innerHTML = equations[equationNum][1];
 
-    if(questionNum<10)
-    {
-	    document.getElementById("i1").style.visibility = "hidden";
-	    document.getElementById("i2").style.visibility = "hidden";
-    }
-
     addInputEventListeners();
 
     if(ifResetScrolling) window.scrollTo(0, 0);
     enableDisableButtons();
-}
-
-function changeSelect()
-{
-	switch(document.getElementById("i0").value)
-	{
-		case "Translation":
-			document.getElementById("exp1").innerHTML = "";
-			document.getElementById("exp2").innerHTML = "units in the X direction and ";
-			document.getElementById("exp3").innerHTML = "units in the Y direction";
-			document.getElementById("i1").style.visibility = "visible";
-			document.getElementById("i2").style.visibility = "visible";
-			break;
-		case "Reflection":
-			document.getElementById("exp1").innerHTML = "about the";
-			document.getElementById("exp2").innerHTML = "axis";
-			document.getElementById("exp3").innerHTML = "";
-			document.getElementById("i1").style.visibility = "visible";
-			document.getElementById("i2").style.visibility = "hidden";
-			break;
-		case "Rotation":
-			document.getElementById("exp1").innerHTML = "of";
-			document.getElementById("exp2").innerHTML = "degrees";
-			document.getElementById("exp3").innerHTML = "";
-			document.getElementById("i1").style.visibility = "visible";
-			document.getElementById("i2").style.visibility = "hidden";
-			break;
-		case "Enlargement":
-			document.getElementById("exp1").innerHTML = "by a factor of";
-			document.getElementById("exp2").innerHTML = "";
-			document.getElementById("exp3").innerHTML = "";
-			document.getElementById("i1").style.visibility = "visible";
-			document.getElementById("i2").style.visibility = "hidden";
-			break;
-	}
 }
 
 function giveHint()
@@ -145,31 +105,6 @@ function giveHint()
         }
         checkChanged(input);
     }
-}
-
-function translate(pos, x, y)
-{
-	return [[pos[0][0]+x, pos[0][1]+y], [pos[1][0]+x, pos[1][1]+y], [pos[2][0]+x, pos[2][1]+y], [pos[3][0]+x, pos[3][1]+y], [pos[4][0]+x, pos[4][1]+y]];
-}
-
-function reflectX(pos)
-{
-	return [[pos[0][0], -pos[0][1]], [pos[1][0], -pos[1][1]], [pos[2][0], -pos[2][1]], [pos[3][0], -pos[3][1]], [pos[4][0], -pos[4][1]]];
-}
-
-function reflectY(pos)
-{
-	return [[-pos[0][0], pos[0][1]], [-pos[1][0], pos[1][1]], [-pos[2][0], pos[2][1]], [-pos[3][0], pos[3][1]], [-pos[4][0], pos[4][1]]];
-}
-
-function rotate(pos, angle)
-{
-	return [[(pos[0][0]*Math.cos(angle))+(pos[0][1]*Math.sin(angle)), (-pos[0][0]*Math.sin(angle))+(pos[1][1]*Math.cos(angle))], [(pos[1][0]*Math.cos(angle))+(pos[1][1]*Math.sin(angle)), (-pos[1][0]*Math.sin(angle))+(pos[1][1]*Math.cos(angle))],[(pos[2][0]*Math.cos(angle))+(pos[2][1]*Math.sin(angle)), (-pos[2][0]*Math.sin(angle))+(pos[2][1]*Math.cos(angle))],[(pos[3][0]*Math.cos(angle))+(pos[3][1]*Math.sin(angle)), (-pos[3][0]*Math.sin(angle))+(pos[3][1]*Math.cos(angle))],[(pos[4][0]*Math.cos(angle))+(pos[4][1]*Math.sin(angle)), (-pos[4][0]*Math.sin(angle))+(pos[4][1]*Math.cos(angle))]];
-}
-
-function enlarge(pos, factor)
-{
-	return [[factor*pos[0][0], factor*pos[0][1]], [factor*pos[1][0], factor*pos[1][1]], [factor*pos[2][0], factor*pos[2][1]], [factor*pos[3][0], factor*pos[3][1]], [factor*pos[4][0], factor*pos[4][1]]];
 }
 
 var numQuestions = 10;
